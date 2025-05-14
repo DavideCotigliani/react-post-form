48a35f25bfb124e4b9a66db32b169543968ceca2
DavideCotigliani <davide.cotigliani@gmail.com>
2025-05-14 17:18:09 +0200

    [UPD] handled the submit of the form and handled the call AJAX on axios

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ function App() {
 
   // variabile di stato che sarà un oggetto
   const [formData, setFormData] = useState({
-    name: "",
+    author: "",
     title: "",
-    text: "",
-    declaration: false,
+    body: "",
+    public: false,
   });
   // definisco la funzione per i cambiamenti dei valori input
   function handleFormData(e) {
@@ -21,29 +21,34 @@ function App() {
   }
   // svolgo la chiamata AJAX all'endpoin
   const handleSubmit = (e) => {
-
+    e.preventDefault();
+    axios.post("https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts", formData).then((resp) => {
+      console.log(resp.data)
+    })
   };
   return (
     <>
-      <div className="container">
-        <form action="">
+      <div className="container mt-5">
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col 6 my-3">
-              <input type="text" name='name' value={formData.name} onChange={handleFormData} placeholder='Nome autore' />
+              <input type="text" name='author' value={formData.author} onChange={handleFormData} placeholder='Nome autore' />
             </div>
             <div className="col 6 my-3">
               <input type="text" name='title' value={formData.title} onChange={handleFormData} placeholder='Titolo del post' />
             </div>
           </div>
           <div className="row">
-            <div className="col 4">
-              <textarea name="text" value={formData.text} onChange={handleFormData} placeholder='Inserisci il testo'></textarea>
+            <div className="col 6">
+              <textarea name="body" value={formData.body} onChange={handleFormData} placeholder='Inserisci il testo'></textarea>
             </div>
-            <div className="col 4">
-              <input type="checkbox" checked={formData.declaration} onChange={handleFormData} /> Post pubblico
-              <input type="checkbox" checked={formData.declaration} onChange={handleFormData} /> Bozza
+            <div className="col 6">
+              <input type="checkbox" checked={formData.public === true} name="public" onChange={() => setFormData({ ...formData, public: true })} /> Post pubblico
+              <input type="checkbox" checked={formData.public === false} name="public" onChange={() => setFormData({ ...formData, public: false })} /> Bozza
             </div>
-            <div className="col 4">
+          </div>
+          <div className="row">
+            <div className="col 12">
               <button>Invia</button>
             </div>
           </div>
