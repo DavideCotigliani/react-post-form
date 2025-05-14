59bc3eee327be19d4bb47699e0a1239322ed8b4b
DavideCotigliani <davide.cotigliani@gmail.com>
2025-05-14 15:41:20 +0200

    [UPD] Created the useState that will be an object and added the value to properties and the attribute name

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
 import { useState } from 'react'
 import axios from 'axios'
 
+// variabile di stato che sarà un oggetto
+const [formData, useFormData] = useState({
+  name: "",
+  title: "",
+  text: "",
+  declaration: false,
+});
+
 function App() {
 
   return (
     <>
       <div className="container">
-        <div className="row">
-          <div className="col 6 my-3">
-            <input type="text" placeholder='Nome autore' />
+        <form action="">
+          <div className="row">
+            <div className="col 6 my-3">
+              <input type="text" name='name' value={formData.name} placeholder='Nome autore' />
+            </div>
+            <div className="col 6 my-3">
+              <input type="text" name='title' value={formData.title} placeholder='Titolo del post' />
+            </div>
           </div>
-          <div className="col 6 my-3">
-            <input type="text" placeholder='Titolo del post' />
+          <div className="row">
+            <div className="col 4">
+              <textarea name="text" value={formData.text} placeholder='Inserisci il testo'></textarea>
+            </div>
+            <div className="col 4">
+              <input type="checkbox" /> Post pubblico
+              <input type="checkbox" /> Bozza
+            </div>
+            <div className="col 4">
+              <button>Invia</button>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col 6">
-            <textarea name="" id="" placeholder='Inserisci il testo'></textarea>
-          </div>
-          <div className="col 6">
-            <input type="checkbox" /> Post pubblico
-            <input type="checkbox" /> Bozza
-          </div>
-        </div>
+        </form>
       </div>
     </>
   )
