9a8f688af1e6a71a992f6668b614fbc80b487253
DavideCotigliani <davide.cotigliani@gmail.com>
2025-05-14 15:24:39 +0200

    [UPD] Created a static form and imported axios

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,25 @@ function App() {
 
   return (
     <>
-
+      <div className="container">
+        <div className="row">
+          <div className="col 6 my-3">
+            <input type="text" placeholder='Nome autore' />
+          </div>
+          <div className="col 6 my-3">
+            <input type="text" placeholder='Titolo del post' />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col 6">
+            <textarea name="" id="" placeholder='Inserisci il testo'></textarea>
+          </div>
+          <div className="col 6">
+            <input type="checkbox" /> Post pubblico
+            <input type="checkbox" /> Bozza
+          </div>
+        </div>
+      </div>
     </>
   )
 }
